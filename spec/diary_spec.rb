3773f260d80d534d92fff6ee407e14b9ad947940
James Ault <james.ault@johnlewis.co.uk>
2019-02-14 13:42:15 +0000

    Exercise 4: Added missed index test

--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -9,4 +9,17 @@ describe Diary do
   diary.add(title,body)
   expect(diary.instance_variable_get(:@entries)).to include entry_double
   end
+
+  it 'Returns the list of titles when index is called' do
+    title = double :title
+    body = double :body
+    entry_double = double :entry, title: title, body: body
+    entry_class_double = double :entry_class, new: entry_double
+    diary = Diary.new(entry_class_double)
+    diary.add(title,body)
+    diary.add(title,body)
+    diary.add(title,body)
+    diary.add(title,body)
+    expect(diary.index).to eq "#{title}\n#{title}\n#{title}\n#{title}"
+  end
 end
